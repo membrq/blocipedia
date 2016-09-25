77dedc2c113bce6d9b06911b29398fd5575d5793
Marie-Elise <membrq@gmail.com>
2016-09-25 17:54:56 -0500

    Checkpoint 3 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-          :rememberable, :trackable, :validatable, :confirmable 
+          :rememberable, :trackable, :validatable, :confirmable
+
+          def avatar_url(size)
+          gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+          "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  #authenticated :user do
+  #  root "users#show", as: :authenticated_root
+  #end
+
+  resources :users, only: [:show]
 
   root 'welcome#index'
 end
